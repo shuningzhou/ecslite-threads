1a1b7bad8722afb964620ae1d76e028ec92d0de9
Leopotam <leopotam@gmail.com>
2021-05-22 03:04:45 +0300

    * CoreApi updated.

--- src/systems.cs
+++ src/systems.cs
@@ -135,7 +135,7 @@ namespace Leopotam.EcsLite.Threads {
         where T1 : struct {
         void Init (
             int[] entities,
-            EcsPool<T1>.PoolItem[] pool);
+            T1[] pool);
     }
 
     public interface IEcsThread<T1, T2> : IEcsThreadBase
@@ -143,8 +143,8 @@ namespace Leopotam.EcsLite.Threads {
         where T2 : struct {
         void Init (
             int[] entities,
-            EcsPool<T1>.PoolItem[] pool1,
-            EcsPool<T2>.PoolItem[] pool2);
+            T1[] pool1,
+            T2[] pool2);
     }
 
     public interface IEcsThread<T1, T2, T3> : IEcsThreadBase
@@ -153,9 +153,9 @@ namespace Leopotam.EcsLite.Threads {
         where T3 : struct {
         void Init (
             int[] entities,
-            EcsPool<T1>.PoolItem[] pool1,
-            EcsPool<T2>.PoolItem[] pool2,
-            EcsPool<T3>.PoolItem[] pool3);
+            T1[] pool1,
+            T2[] pool2,
+            T3[] pool3);
     }
 
     public interface IEcsThread<T1, T2, T3, T4> : IEcsThreadBase
@@ -165,9 +165,9 @@ namespace Leopotam.EcsLite.Threads {
         where T4 : struct {
         void Init (
             int[] entities,
-            EcsPool<T1>.PoolItem[] pool1,
-            EcsPool<T2>.PoolItem[] pool2,
-            EcsPool<T3>.PoolItem[] pool3,
-            EcsPool<T4>.PoolItem[] pool4);
+            T1[] pool1,
+            T2[] pool2,
+            T3[] pool3,
+            T4[] pool4);
     }
 }
